2c20752b2988bf6e33d5fb186e612e68badb0c03
chawki <achouriichawkii@gmail.com>
2024-11-01 22:56:34 +0100

    good nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/app.css";
 import Nav from "./nav/Nav.js";
+import Home from "./home/Home.js";
+import Services from "./services/Services.js";
+import About from "./about/About.js";
+import Contact from "./contact/Contact.js";
 
 const App = () => {
   return (
     <Router>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Services />} />
+        <Route path="/projects" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 };
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,57 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/nav.css";
 
 const Nav = () => {
-  const [isMenuVisible, setMenuVisible] = useState(false);
-  const [activeDropdown, setActiveDropdown] = useState(null);
-
-  const toggleMenu = () => {
-    setMenuVisible(!isMenuVisible);
-  };
+  const [showMenu, setShowMenu] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(null);
 
   const toggleDropdown = (index) => {
-    setActiveDropdown(activeDropdown === index ? null : index);
+    setShowDropdown((prevIndex) => (prevIndex === index ? null : index));
   };
+
+  const handleResize = () => {
+    if (window.innerWidth >= 1118) {
+      setShowDropdown(null);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <header className="header">
       <nav className="nav container">
         <div className="nav__data">
           <a className="nav__logo">Logo</a>
-          <div className="nav__toggle" onClick={toggleMenu}>
+          <div
+            className="nav__toggle"
+            onClick={() => setShowMenu(!showMenu)}
+            id="nav-toggle"
+          >
             <i
               className={`ri-menu-line nav__toggle-menu ${
-                isMenuVisible ? "hide" : ""
+                showMenu ? "hide-icon" : "show-icon"
               }`}
             ></i>
             <i
               className={`ri-close-line nav__toggle-close ${
-                isMenuVisible ? "" : "hide"
+                showMenu ? "show-icon" : "hide-icon"
               }`}
             ></i>
           </div>
         </div>
 
-        <div className={`nav__menu ${isMenuVisible ? "show-menu" : ""}`}>
+        <div
+          className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
           <ul className="nav__list">
             <li>
               <a href="#" className="nav__link">
                 Home
               </a>
             </li>
-
             <li className="dropdown__item">
               <div
                 className="nav__link dropdown__button"
                 onClick={() => toggleDropdown(0)}
+                aria-expanded={showDropdown === 0}
+                role="button"
+                tabIndex={0}
               >
                 Services{" "}
-                <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+                <i
+                  className={`ri-arrow-down-s-line dropdown__arrow ${
+                    showDropdown === 0 ? "rotate" : ""
+                  }`}
+                ></i>
               </div>
 
               <div
                 className={`dropdown__container ${
-                  activeDropdown === 0 ? "show-dropdown" : ""
+                  showDropdown === 0 ? "show-dropdown" : ""
                 }`}
               >
                 <div className="dropdown__content">
@@ -59,7 +81,7 @@ const Nav = () => {
                     <div className="dropdown__icon">
                       <i className="ri-flashlight-line"></i>
                     </div>
-                    <span className="dropdown__title">Shop</span>
+                    <span className="dropdown__title">shop</span>
                     <ul className="dropdown__list">
                       <li>
                         <a href="#" className="dropdown__link">
@@ -73,7 +95,6 @@ const Nav = () => {
                       </li>
                     </ul>
                   </div>
-
                   <div className="dropdown__group">
                     <div className="dropdown__icon">
                       <i className="ri-shield-line"></i>
@@ -95,7 +116,6 @@ const Nav = () => {
                 </div>
               </div>
             </li>
-
             <li>
               <a href="#" className="nav__link">
                 About
